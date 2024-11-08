1ea5161478fe8c9433ede1d558a6df445307bdde
mahaandika <agungandikabali@gmail.com>
2024-11-08 10:55:37 +0800

    add sponsor section on the home page

--- src/scenes/home/Home.tsx
+++ src/scenes/home/Home.tsx
@@ -56,9 +56,9 @@ const Home = ({ setSelectedPage }: Props) => {
 
       {/* sponsor */}
       {isAboveMediumScreen && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between">
               <img src={SponsorRedbull} alt="" />
               <img src={SponsorForbes} alt="" />
               <img src={SponsorFortune} alt="" />
