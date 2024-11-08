d23a50e85de10b41b1de0136bbe0c804e6f23976
mahaandika <agungandikabali@gmail.com>
2024-11-08 20:46:41 +0800

    finish the benefits page

--- src/scenes/benefits/Benefits.tsx
+++ src/scenes/benefits/Benefits.tsx
@@ -8,6 +8,8 @@ import {
 import { motion, stagger } from "framer-motion";
 import { div } from "framer-motion/client";
 import BenefitsContent from "./BenefitsContent";
+import ActionButton from "@/shared/ActionButton";
+import BenefitPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 const benefits: Array<BenefitType> = [
   {
@@ -87,6 +89,67 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        {/* graphic and description */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* graphic image */}
+          <motion.img
+            src={BenefitPageGraphic}
+            alt=""
+            className="mx-auto"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          />
+
+          {/* description */}
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            {/* title */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                <HeadingTitle>
+                  millions of happy member getting{" "}
+                  <span className="text-primary-500"> fit</span>
+                </HeadingTitle>
+              </div>
+            </div>
+            {/* desc */}
+            <div>
+              <p className="my-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia quod tempore, consequatur reiciendis fuga aspernatur
+                dolorum accusantium possimus voluptatem fugit.
+              </p>
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia quod tempore, consequatur reiciendis fuga aspernatur
+                dolorum accusantium possimus voluptatem fugit.
+              </p>
+            </div>
+            {/* button */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  join now
+                </ActionButton>
+              </div>
+            </div>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   );
